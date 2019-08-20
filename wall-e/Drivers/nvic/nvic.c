0d3928ece6c4d0975334c9c23a3d18db0b69a725
Laurence <thegreaterlee@gmail.com>
2019-08-20 15:20:55 +0800

    Add uart3 nvic configuration.

--- wall-e/Drivers/nvic/nvic.c
+++ wall-e/Drivers/nvic/nvic.c
@@ -1,27 +1,45 @@
 #include "nvic.h"
-
-//设置NVIC 
-//NVIC_PreemptionPriority:抢占优先级
-//NVIC_SubPriority       :响应优先级
-//NVIC_Channel           :中断编号
-//NVIC_Group             :中断分组 0~4
-//注意优先级不能超过设定的组的范围!否则会有意想不到的错误
-//组划分:
-//组0:0位抢占优先级,4位响应优先级
-//组1:1位抢占优先级,3位响应优先级
-//组2:2位抢占优先级,2位响应优先级
-//组3:3位抢占优先级,1位响应优先级
-//组4:4位抢占优先级,0位响应优先级
-//NVIC_SubPriority和NVIC_PreemptionPriority的原则是,数值越小,越优先	   
+   
+/********************************************************************************************************
+Function:void NVIC_Configuration(void)
+Description:NVIC Configuration
+Input:None
+Return:None
+Others:
+	NVIC_Channel           : Interrupt entrance
+	NVIC_PreemptionPriority: Preemption priority
+	NVIC_SubPriority       : Response priority
+	NVIC_Group             : Interrupt group 0~4
+	
+	Group division:
+		Group 0: 0-bit preemption priority,4-bit response priority
+		Group 1: 1-bit preemption priority,3-bit response priority
+		Group 2: 2-bit preemption priority,2-bit response priority
+		Group 3: 3-bit preemption priority,1-bit response priority
+		Group 4: 4-bit preemption priority,0-bit response priority
+	
+	Note:
+		1.The priority cannot exceed the range of the set group,Otherwise there will be unexpected errors.
+		2.NVIC_SubPriority/NVIC_PreemptionPriority: the smaller the value, the higher the priority.
+**********************************************************************************************************/
 void NVIC_Configuration(void)
 {
-		NVIC_InitTypeDef  NVIC_InitStructure;	
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);	//设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	NVIC_InitTypeDef  NVIC_InitStructure;	
+	/* Use NVIC interrupt Group 2: 2-bit preemption priority, 2-bit response priority */
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);	
 
-		//////////////////定时器3的中断优先级配置//////////////////		
-		NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;  //TIM3中断
-		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;  //先占优先级1级
-		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;  //从优先级3级
-		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
-		NVIC_Init(&NVIC_InitStructure);  //根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器	 
+	//////////////////定时器3的中断优先级配置//////////////////		
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;  //TIM3中断
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;  //先占优先级1级
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;  //从优先级3级
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
+	NVIC_Init(&NVIC_InitStructure);  //根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器
+
+	/* Usart3 NVIC interrupt priority configuration (Bluetooth serial port configuration) */
+	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0x02 ;/* Preemption priority: 2 */
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x02;	   /* Response priority: 2 */
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			   /* IRQ channel enable */
+	/* Initialize the peripheral NVIC register according to the parameters specified in NVIC_InitStruct	*/
+	NVIC_Init(&NVIC_InitStructure);	
 }
